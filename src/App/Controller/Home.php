06ab3e90e2e53c2fd8ac0b3e35f811b7b6d64463
Laura Quintana Sais <laura.quintana.sais@gmail.com>
2015-06-28 00:23:07 +0200

    Added Twig template Home controller uses twig Composer lock changed due to twig integration Added twig config params to index.php

--- src/App/Controller/Home.php
+++ src/App/Controller/Home.php
@@ -4,11 +4,32 @@ namespace App\Controller;
 
 use Fw\Component\Controllers\Controller;
 use Fw\Component\Dispatching\HttpRequest;
+use Fw\Component\Dispatching\WebResponse;
+
+
 
 class Home implements Controller {
 
     public function __invoke(HttpRequest $request) {
-        echo 'Hello';
+
+        $name='Eva';
+        $surname='Doménech';
+
+        $result = array('name' => $name, 'last_name' => $surname);
+
+        $template = 'index.html.twig';
+
+        $response = new WebResponse($result, $template);
+
+        return $response;
+
+
+        //$loader = new \Twig_Loader_Filesystem(__DIR__ .'/../../templates' );
+
+        //$twig = new Twig_Environment($loader);
+       // echo $twig->render('index.html.twig', array('name' => $name));
+
+
     }
 
 }
--- web/index.php
+++ web/index.php
@@ -3,78 +3,22 @@
 require '../vendor/autoload.php';
 
 use Fw\Application;
-use Fw\Component\Routing\PhpParser;
-use Fw\Component\Dispatching\HttpDispatcher;
-use Fw\Component\Dispatching\HttpRequest;
-use Fw\Component\Dispatching\Request;
-use App\Controller\Home;
-use App\Controller\Welcome;
-use Fw\Component\Dispatching\JsonResponse;
-use Fw\Component\Dispatching\WebResponse;
-use Fw\Component\Views\JsonView;
-use Fw\Component\Views\WebView;
 
 $application= new Application;
-$routing = new PhpParser();
 
+$twig_fileystem=__DIR__ . '/../src/templates';
 
-if (!isset($_SERVER['PATH_INFO'])) {
-    $route=$_SERVER['REQUEST_URI'];
-} else {
-    $route = $_SERVER['PATH_INFO'];
-}
-
-
-$routes = array(
-    'Home'=> array(
-        'route'=>'/',
-        'method'=>'get'
-    ),
-
-    'Welcome' => array(
-        'route'=>'/welcome',
-        'method'=>'get'
-    ),
+$options = array(
+    'cache' => false,
+    'debug' => true,
+    'autoescape' => false
 );
 
-if (!$route) {
-    $route='/';
-}
+$loader = new \Twig_Loader_Filesystem($twig_fileystem);
 
-$route_name = $routing->parseRoute($route, $routes);
-
-$controllers = array(
-    'Home' => array(
-        'controller'=>'App\Controller\Home'
-    ),
-    'Welcome'=>array(
-        'controller'=>'App\Controller\Welcome'
-    )
-);
+$twig = new \Twig_Environment($loader, $options);
 
 
-$get_controller = new HttpDispatcher;
-
-$controller = $get_controller->dispatchController($route_name, $controllers);
-
-$controller_i = new $controller;
-
-$request = new Request();
-$httprequest = new HttpRequest($request, $_GET, $_POST, $_SERVER);
-
-$response = $controller_i($httprequest);
-
-if ($response instanceof JsonResponse) {
-
-    $result = new JsonView();
-
-   $result->render($response->getResponse());
-
-
-} else if ($response instanceof WebResponse) {
-
-}
-
-
+$application->setTemplateEngine($twig);
 
 $application->run();
